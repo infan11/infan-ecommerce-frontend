7633aba12f7b1d6fc21bb37fbaa46c40d36a388e
infan11 <infanjiounrahman20606@gmail.com>
2024-11-14 07:11:54 +0600

    adde private routes  and admin routes.js

--- src/Components/Hooks/useAxiosSecure.jsx
+++ src/Components/Hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ const useAxiosSecure = () => {
     const { logout } = useAuth(null)
     const navigate = useNavigate();
     const axiosSecure = axios.create({
-        baseURL: "http://localhost:5000"
+        baseURL: "https://infan-ecommerce-backend.vercel.app"
     })
 
    axiosSecure.interceptors.request.use(function (config) {
