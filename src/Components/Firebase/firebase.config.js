83a0a09a0a0238d6eb85e207599ac3ad5bf9b4e8
infan11 <infanjiounrahman20606@gmail.com>
2024-11-14 07:01:04 +0600

    adde private routes  and admin routes.js

--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBJSFXibm82wSW2cfacARBgUNtUvpSJWv0",
-  authDomain: "infan-web.firebaseapp.com",
-  projectId: "infan-web",
-  storageBucket: "infan-web.appspot.com",
-  messagingSenderId: "1070992863724",
-  appId: "1:1070992863724:web:0115c58feb01b47e0b90c9",
-  measurementId: "G-969MWPH5N2"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId
+
 };
 
 // Initialize Firebase
